3982d506119b3c06e314e2641d8a3317293b36d8
frederic4 <55210942+frederic4@users.noreply.github.com>
2019-11-27 21:32:53 +0100

    Add files via upload

---
+++ le_pendu_liste.py
@@ -0,0 +1,13 @@
+# ce fichier contient les mots de liste pour le jeu du pendu
+
+
+liste_mots = [
+    "petit",
+    "grand",
+    "voiture",
+    "orange",
+    "kiwi",
+    "camion",
+    "cheval",
+    "canapé",
+    ]
